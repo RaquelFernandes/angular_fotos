b9ed43d1286cfed7b47ace21eb14c9966e28db7e
RaquelFernandes <raquelsol-@hotmail.com>
2017-10-29 17:21:14 -0200

    Segura Essa Dani Boy

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,7 @@
+angular.module('alurapic').controller('FotosController', function($scope){
+
+        $scope.foto = {
+            url:"https://www.baixarvideosgratis.com.br/imagens/entretenimento/animais/gatinho-fofinho-pedindo-petisco-tem-como-nao-amar-estes-seres.jpg",
+            titulo:"gatinho fofinho pedindo petisco"
+        };
+})
